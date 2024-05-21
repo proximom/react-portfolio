e2ce977f394b862b8c0d21de3fac7529557d1d8e
Muhammad Ahsan Farooq <ahsan.farooq531@gmail.com>
2024-05-21 21:13:27 +0500

    final idc

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,12 +5,12 @@ import { FaJs, FaReact, FaCss3Alt, FaHtml5, FaDatabase, FaPython, FaMicrosoft, F
 
 const skills = [
   { name: 'JavaScript', level: 90, icon: <FaJs />, animation: 'fade-left' },
-  { name: 'React', level: 80, icon: <FaReact />, animation: 'fade-right' },
-  { name: 'CSS', level: 75, icon: <FaCss3Alt />, animation: 'fade-left' },
-  { name: 'HTML', level: 60, icon: <FaHtml5 />, animation: 'fade-right' },
+  { name: 'React', level: 50, icon: <FaReact />, animation: 'fade-right' },
+  { name: 'CSS', level: 40, icon: <FaCss3Alt />, animation: 'fade-left' },
+  { name: 'HTML', level: 55, icon: <FaHtml5 />, animation: 'fade-right' },
   { name: 'Database', level: 88, icon: <FaDatabase />, animation: 'fade-left' },
-  { name: 'Python', level: 50, icon: <FaPython />, animation: 'fade-right' },
-  { name: 'C#', level: 70, icon: <FaMicrosoft />, animation: 'fade-left' },
+  { name: 'Python', level: 20, icon: <FaPython />, animation: 'fade-right' },
+  { name: 'C#', level: 60, icon: <FaMicrosoft />, animation: 'fade-left' },
   { name: '.NET', level: 90, icon: <FaDotCircle />, animation: 'fade-right' },
   { name: 'Flutter', level: 90, icon: <FaMobileAlt />, animation: 'fade-left' },
 ];
@@ -26,7 +26,7 @@ const Skills = () => {
             <h3>{skill.name}</h3>
             <div className="progress-bar">
               <div className="progress" style={{ width: `${skill.level}%` }}>
-                <span>{skill.level}%</span>
+                <span> </span>
               </div>
             </div>
           </div>
